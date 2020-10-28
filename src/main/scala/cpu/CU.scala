c20ee2f6370408b07fafa511004957a2de023909
sinofp <sinofp@tuta.io>
2020-10-28 11:08:39 +0800

    Add Instructions, CU (for test)
    这个BitPat是个好东西，可以方便的===，但不能和switch使用，怪
    指令的格式、对应控制位一定是要写的，连接它们的可以是when，但写个函数连起来应该更好
    
    rocket-chip自己写了个求解解码器的代码，有人提出要合并进chisel，但我看那issue已经一年没动静了……
    从rocket那里抠出来也不是不可以，但感觉没啥必要

--- src/main/scala/cpu/CU.scala
+++ src/main/scala/cpu/CU.scala
@@ -3,26 +3,48 @@
 package cpu
 
 import chisel3._
+import chisel3.stage.ChiselStage
+import chisel3.util._
+import cpu.Instructions.ADD
 
 object CU {
+  // @formatter:off
   // BR
   val SZ_BR_TYPE = 3.W
-
   def isBeq = (x: UInt) => x === 1.U(SZ_BR_TYPE)
-
   def isBne = (x: UInt) => x === 2.U(SZ_BR_TYPE)
-
   def isBgez = (x: UInt) => x === 3.U(SZ_BR_TYPE)
-
   def isBgtz = (x: UInt) => x === 4.U(SZ_BR_TYPE)
-
   def isBlez = (x: UInt) => x === 5.U(SZ_BR_TYPE)
-
   def isBltz = (x: UInt) => x === 6.U(SZ_BR_TYPE)
-
+  // @formatter:on
 }
+
+import cpu.Instructions._
 
 class CU extends Module {
   val io = IO(new Bundle() {
+    val inst = Input(UInt(32.W))
+    val out = Output(UInt(32.W))
   })
+
+  import io._
+
+  private def =?(bp: BitPat) = inst === bp
+
+  when(=?(ADD)) {
+    out := 1.U
+  }.elsewhen(=?(ADDI)) {
+    out := 2.U
+  }.elsewhen(=?(ADDIU)) {
+    out := 3.U
+  }.elsewhen(=?(J)) {
+    out := 4.U
+  }.otherwise {
+    out := 4.U
+  }
+}
+
+object CU1 extends App {
+  (new ChiselStage).emitVerilog(new CU)
 }
