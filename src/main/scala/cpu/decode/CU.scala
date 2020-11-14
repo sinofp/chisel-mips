84fea54e8108ad60cc69613e870ba106be9f1941
sinofp <sinofp@tuta.io>
2020-11-14 23:27:18 +0800

    Clean up CU
    1. 写个函数包裹List，这样IDE会提示写到哪个参数了，以及0/1是给哪个参数的
    2. CtrlSigs里设置ABC_X的默认信号，同样是为了清楚（但是好长啊！）
    3. 把BitPat重命名为B，主要为了减短1中的函数声明长度。
    
    想要个能把table里空格对齐的格式化工具

--- src/main/scala/cpu/decode/CU.scala
+++ src/main/scala/cpu/decode/CU.scala
@@ -3,7 +3,7 @@
 package cpu.decode
 
 import chisel3._
-import chisel3.util._
+import chisel3.util.{BitPat => B}
 import cpu.decode.Instructions._
 import cpu.execute.ALU._
 import cpu.port.HILOWen
@@ -14,6 +14,7 @@ object CtrlSigDef {
   // Br
   val SZ_BR_TYPE = 3.W
   val BR_TYPE_NO = 0.U(SZ_BR_TYPE)
+  val BR_TYPE_X = BR_TYPE_NO
   val BR_TYPE_EQ = 1.U(SZ_BR_TYPE)
   val BR_TYPE_NE = 2.U(SZ_BR_TYPE)
   val BR_TYPE_GE = 3.U(SZ_BR_TYPE)
@@ -24,6 +25,7 @@ object CtrlSigDef {
   // Mem
   val SZ_MEM_TYPE = 2.W
   val MEM_WORD = 0.U(SZ_MEM_TYPE)
+  val MEM_X = MEM_WORD
   val MEM_HALF = 1.U(SZ_MEM_TYPE)
   val MEM_BYTE = 2.U(SZ_MEM_TYPE)
 
@@ -41,14 +43,16 @@ object CtrlSigDef {
   val FORWARD_HILO_WB = 2.U(SZ_FORWARD_HILO)
 
   // Select
-  val SZ_SEL_ALU1 = 2.W
-  val SEL_ALU1_SA = 0.U(SZ_SEL_ALU1) // todo 没别的就改成Bool()
+  val SZ_SEL_ALU1 = 1.W
+  val SEL_ALU1_SA = 0.U(SZ_SEL_ALU1)
   val SEL_ALU1_RS = 1.U(SZ_SEL_ALU1)
+  val SEL_ALU1_X = SEL_ALU1_RS
 
   val SZ_SEL_ALU2 = 2.W
   val SEL_ALU2_IMM = 0.U(SZ_SEL_ALU2)
   val SEL_ALU2_RT = 1.U(SZ_SEL_ALU2)
   val SEL_ALU2_ZERO = 2.U(SZ_SEL_ALU2)
+  val SEL_ALU2_X = SEL_ALU2_RT
 
   val SZ_SEL_IMM = 3.W
   val SEL_IMM_U = 0.U(SZ_SEL_IMM)
@@ -56,11 +60,13 @@ object CtrlSigDef {
   val SEL_IMM_B = 2.U(SZ_SEL_IMM) // USB!
   val SEL_IMM_J = 3.U(SZ_SEL_IMM)
   val SEL_IMM_SH = 4.U(SZ_SEL_IMM)
+  val SEL_IMM_X = SEL_IMM_U
 
   val SZ_SEL_REG_WADDR = 2.W
   val SEL_REG_WADDR_RD = 0.U(SZ_SEL_REG_WADDR)
   val SEL_REG_WADDR_RT = 1.U(SZ_SEL_REG_WADDR)
   val SEL_REG_WADDR_31 = 2.U(SZ_SEL_REG_WADDR)
+  val SEL_REG_WADDR_X = SEL_REG_WADDR_RD
 
   val SZ_SEL_REG_WDATA = 3.W
   val SEL_REG_WDATA_ALU = 0.U(SZ_SEL_REG_WDATA)
@@ -68,10 +74,7 @@ object CtrlSigDef {
   val SEL_REG_WDATA_LNK = 2.U(SZ_SEL_REG_WDATA)
   val SEL_REG_WDATA_HI = 3.U(SZ_SEL_REG_WDATA)
   val SEL_REG_WDATA_LO = 4.U(SZ_SEL_REG_WDATA)
-
-  val X = BitPat(0.U)
-  val XX = BitPat(0.U(2.W))
-  val XXX = BitPat(0.U(3.W))
+  val SEL_REG_WDATA_X = SEL_REG_WDATA_ALU
 }
 // @formatter:on
 
@@ -93,26 +96,34 @@ class CtrlSigs extends Bundle with HILOWen {
   val load = Bool()
   // todo ...?
 
-  implicit def uint2BitPat(x: UInt): BitPat = BitPat(x)
+  private implicit def uint2B(x: UInt): B = B(x)
 
-  implicit def int2BitPat(x: Int): BitPat = BitPat(x.U)
+  private implicit def int2B(x: Int): B = B(x.U)
 
-  private val default: List[BitPat] = List(SEL_ALU1_RS, SEL_ALU2_RT, XXX, FN_X, 0, 0, 0, 0, XX, XX, XXX, MEM_WORD, X, X, X)
+  private val default: List[B] = l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 0, 0)
 
-  private val table: Array[(BitPat, List[BitPat])] = Array(
-    ADD -> List(SEL_ALU1_RS, SEL_ALU2_RT, XXX, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, XXX, XX, 0, 0, 0),
-    ADDI -> List(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, XXX, XX, 0, 0, 0),
-    SUB -> List(SEL_ALU1_RS, SEL_ALU2_RT, XXX, FN_SUB, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, XXX, XX, 0, 0, 0),
-    BEQ -> List(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, XX, XXX, BR_TYPE_EQ, 0, 0, 0),
-    BLTZ -> List(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, XX, XXX, BR_TYPE_LT, 0, 0, 0),
-    LW -> List(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_WORD, 1, 0, 0),
-    SW -> List(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 1, 0, XX, XXX, BR_TYPE_NO, MEM_WORD, 0, 0, 0),
-    MTHI -> List(SEL_ALU1_RS, XX, XXX, FN_X, 0, 0, 0, 0, XX, XXX, XXX, XX, 0, 1, 0),
-    MTLO -> List(SEL_ALU1_RS, XX, XXX, FN_X, 0, 0, 0, 0, XX, XXX, XXX, XX, 0, 0, 1),
-    MFHI -> List(XX, XX, XXX, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_HI, XXX, XX, 0, 0, 0),
-    MFLO -> List(XX, XX, XXX, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_LO, XXX, XX, 0, 0, 0),
-    MULT -> List(SEL_ALU1_RS, SEL_ALU2_RT, XXX, FN_MULT, 1, 0, 0, 0, XX, XXX, XXX, XX, 0, 1, 1),
+  private val table: Array[(B, List[B])] = Array(
+    ADD -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
+    ADDI -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
+    SUB -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SUB, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
+    BEQ -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_EQ, MEM_X, 0, 0, 0),
+    BLTZ -> l(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_LT, MEM_X, 0, 0, 0),
+    LW -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_WORD, 1, 0, 0),
+    SW -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 1, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_NO, MEM_WORD, 0, 0, 0),
+    MTHI -> l(SEL_ALU1_RS, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 0),
+    MTLO -> l(SEL_ALU1_RS, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 0, 1),
+    MFHI -> l(SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_HI, BR_TYPE_X, MEM_X, 0, 0, 0),
+    MFLO -> l(SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_LO, BR_TYPE_X, MEM_X, 0, 0, 0),
+    DIV -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_DIV, 0, 1, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
+    DIVU -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_DIVU, 0, 1, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
+    MULT -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_MULT, 1, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
+    MULTU -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_MULTU, 1, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
   )
+
+  // 为了IDE的参数提醒
+  private def l(sel_alu1: B, sel_alu2: B, sel_imm: B, alu_fn: B, mul: B, div: B, mem_wen: B, reg_wen: B, sel_reg_waddr: B, sel_reg_wdata: B, br_type: B, mem_size: B, load: B, hi_wen: B, lo_wen: B): List[B] = {
+    List(sel_alu1, sel_alu2, sel_imm, alu_fn, mul, div, mem_wen, reg_wen, sel_reg_waddr, sel_reg_wdata, br_type, mem_size, load, hi_wen, lo_wen)
+  }
 
   def decode(inst: UInt): this.type = {
     val decoder = DecodeLogic(inst, default, table)
