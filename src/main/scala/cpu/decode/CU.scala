76a69c4cc3b7aeec1e3109c5515a0d1518934834
sinofp <sinofp@tuta.io>
2020-11-19 15:00:17 +0800

    Refine CtrlSigs.CU

--- src/main/scala/cpu/decode/CU.scala
+++ src/main/scala/cpu/decode/CU.scala
@@ -104,66 +104,83 @@ class CtrlSigs extends Bundle with HILOWen {
 
   private implicit def int2B(x: Int): B = B(x.U)
 
-  private val default: List[B] = l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 0, 0)
+  private val default: List[B] = d(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 0, 0)
 
   private val table: Array[(B, List[B])] = Array(
-    ADD -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    ADDI -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    ADDU -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    ADDIU -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SUB -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SUB, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SUBU -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SUB, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SLT -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SLT, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SLTI -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_SLT, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SLTU -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SLTU, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SLTIU -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_SLTU, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    DIV -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_DIV, 0, 1, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
-    DIVU -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_DIVU, 0, 1, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
-    MULT -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_MULT, 1, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
-    MULTU -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_MULTU, 1, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 1),
-    AND -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_AND, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    ANDI -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_AND, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    LUI -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_U, FN_SL, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    NOR -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_OR, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0, 1),
-    OR -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_OR, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    ORI -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_OR, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    XOR -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_XOR, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    XORI -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_XOR, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SLLV -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SL, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SLL -> l(SEL_ALU1_SA, SEL_ALU2_RT, SEL_IMM_SH, FN_SL, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SRAV -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SRA, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SRA -> l(SEL_ALU1_SA, SEL_ALU2_RT, SEL_IMM_SH, FN_SRA, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SRLV -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, FN_SR, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    SRL -> l(SEL_ALU1_SA, SEL_ALU2_RT, SEL_IMM_SH, FN_SR, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0),
-    BEQ -> l(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_NE, MEM_X, 0, 0, 0),
-    BGEZ -> l(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_GE, MEM_X, 0, 0, 0),
-    BGTZ -> l(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_GT, MEM_X, 0, 0, 0),
-    BLEZ -> l(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_LE, MEM_X, 0, 0, 0),
-    BLTZ -> l(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_LT, MEM_X, 0, 0, 0),
-    BGEZAL -> l(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 1, SEL_REG_WADDR_31, SEL_REG_WDATA_LNK, BR_TYPE_GE, MEM_X, 0, 0, 0),
-    BLTZAL -> l(SEL_ALU1_RS, SEL_ALU2_ZERO, SEL_IMM_B, FN_SLT, 0, 0, 0, 1, SEL_REG_WADDR_31, SEL_REG_WDATA_LNK, BR_TYPE_LT, MEM_X, 0, 0, 0),
-    J -> l(SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_J, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 0, 0),
-    JR -> l(SEL_ALU1_SA, SEL_ALU2_X, SEL_IMM_J, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 0, 0),
-    JAL -> l(SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_J, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_31, SEL_REG_WDATA_LNK, BR_TYPE_X, MEM_X, 0, 0, 0),
-    JALR -> l(SEL_ALU1_SA, SEL_ALU2_X, SEL_IMM_J, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_31, SEL_REG_WDATA_LNK, BR_TYPE_X, MEM_X, 0, 0, 0),
-    MFHI -> l(SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_HI, BR_TYPE_X, MEM_X, 0, 0, 0),
-    MFLO -> l(SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_LO, BR_TYPE_X, MEM_X, 0, 0, 0),
-    MTHI -> l(SEL_ALU1_RS, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 1, 0),
-    MTLO -> l(SEL_ALU1_RS, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, 0, 1),
-    LB -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_B, 1, 0, 0),
-    LBU -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_BU, 1, 0, 0),
-    LH -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_H, 1, 0, 0),
-    LHU -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_HU, 1, 0, 0),
-    LW -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_W, 1, 0, 0),
-    SB -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 1, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_NO, MEM_B, 0, 0, 0),
-    SH -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 1, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_NO, MEM_H, 0, 0, 0),
-    SW -> l(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 1, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_NO, MEM_W, 0, 0, 0),
+    ADD -> r(FN_ADD),
+    ADDI -> i(FN_ADD),
+    ADDU -> r(FN_ADD),
+    ADDIU -> r(FN_ADD),
+    SUB -> r(FN_SUB),
+    SUBU -> r(FN_SUB),
+    SLT -> r(FN_SLT),
+    SLTI -> i(FN_SLT),
+    SLTU -> r(FN_SLTU),
+    SLTIU -> i(FN_SLTU),
+    DIV -> m(FN_DIV, div = 1),
+    DIVU -> m(FN_DIVU, div = 1),
+    MULT -> m(FN_MULT, mul = 1),
+    MULTU -> m(FN_MULTU, mul = 1),
+    AND -> r(FN_AND),
+    ANDI -> i(FN_AND),
+    LUI -> i(FN_SL, lui = true),
+    NOR -> r(FN_OR, 1),
+    OR -> r(FN_OR),
+    ORI -> i(FN_OR),
+    XOR -> r(FN_XOR),
+    XORI -> i(FN_XOR),
+    SLLV -> sft(FN_SL, v = true),
+    SLL -> sft(FN_SL),
+    SRAV -> sft(FN_SRA, v = true),
+    SRA -> sft(FN_SRA),
+    SRLV -> sft(FN_SR),
+    SRL -> sft(FN_SR, v = true),
+    BEQ -> b(BR_TYPE_EQ),
+    BGEZ -> b(BR_TYPE_GE),
+    BGTZ -> b(BR_TYPE_GT),
+    BLEZ -> b(BR_TYPE_LE),
+    BLTZ -> b(BR_TYPE_LT),
+    BGEZAL -> b(BR_TYPE_GE, 1),
+    BLTZAL -> b(BR_TYPE_LT, 1),
+    J -> j(),
+    JR -> j(r = true),
+    JAL -> j(l = 1),
+    JALR -> j(r = true, 1),
+    MFHI -> mf(),
+    MFLO -> mf(false),
+    MTHI -> mt(),
+    MTLO -> mt(0),
+    LB -> l(MEM_B),
+    LBU -> l(MEM_BU),
+    LH -> l(MEM_H),
+    LHU -> l(MEM_HU),
+    LW -> l(MEM_W),
+    SB -> s(MEM_B),
+    SH -> s(MEM_H),
+    SW -> s(MEM_W),
   )
 
-  // 为了IDE的参数提醒
-  private def l(sel_alu1: B, sel_alu2: B, sel_imm: B, alu_fn: B, mul: B, div: B, mem_wen: B, reg_wen: B, sel_reg_waddr: B, sel_reg_wdata: B, br_type: B, mem_size: B, load: B, hi_wen: B, lo_wen: B, alu_n: B = 0): List[B] = {
-    List(sel_alu1, sel_alu2, sel_imm, alu_fn, alu_n, mul, div, mem_wen, reg_wen, sel_reg_waddr, sel_reg_wdata, br_type, mem_size, load, hi_wen, lo_wen)
-  }
+  private def r(alu_fn: B, alu_n: B = 0): List[B] = d(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_X, alu_fn, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0, alu_n)
+
+  private def mf(hi: Boolean = true): List[B] = d(SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 1, SEL_REG_WADDR_RD, if (hi) SEL_REG_WDATA_HI else SEL_REG_WDATA_LO, BR_TYPE_X, MEM_X, 0, 0, 0)
+
+  private def mt(hi: Int = 1): List[B] = d(SEL_ALU1_RS, SEL_ALU2_X, SEL_IMM_X, FN_X, 0, 0, 0, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_X, MEM_X, 0, hi, 1 - hi)
+
+  private def i(alu_fn: B, lui: Boolean = false): List[B] = d(SEL_ALU1_RS, SEL_ALU2_IMM, if (lui) SEL_IMM_LUI else SEL_IMM_S, alu_fn, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0)
+
+  private def l(mem_size: B): List[B] = d(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, mem_size, 1, 0, 0)
+
+  private def m(alu_fn: B, div: Int = 0, mul: Int = 0): List[B] = d(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, alu_fn, mul, div, 0, 1, SEL_REG_WADDR_RT, SEL_REG_WDATA_MEM, BR_TYPE_NO, MEM_X, 1, 0, 0)
+
+  private def d(sel_alu1: B, sel_alu2: B, sel_imm: B, alu_fn: B, mul: B, div: B, mem_wen: B, reg_wen: B, sel_reg_waddr: B, sel_reg_wdata: B, br_type: B, mem_size: B, load: B, hi_wen: B, lo_wen: B, alu_n: B = 0): List[B] = List(sel_alu1, sel_alu2, sel_imm, alu_fn, alu_n, mul, div, mem_wen, reg_wen, sel_reg_waddr, sel_reg_wdata, br_type, mem_size, load, hi_wen, lo_wen)
+
+  private def s(mem_size: B): List[B] = d(SEL_ALU1_RS, SEL_ALU2_IMM, SEL_IMM_S, FN_ADD, 0, 0, 1, 0, SEL_REG_WADDR_X, SEL_REG_WDATA_X, BR_TYPE_NO, mem_size, 0, 0, 0)
+
+  private def sft(alu_fn: B, v: Boolean = false): List[B] = d(if (v) SEL_ALU1_RS else SEL_ALU1_SA, SEL_ALU2_RT, SEL_IMM_SH, alu_fn, 0, 0, 0, 1, SEL_REG_WADDR_RD, SEL_REG_WDATA_ALU, BR_TYPE_X, MEM_X, 0, 0, 0)
+
+  private def b(br_type: B, link: Int = 0): List[B] = d(SEL_ALU1_RS, SEL_ALU2_RT, SEL_IMM_B, FN_SLT, 0, 0, 0, link, SEL_REG_WADDR_31, SEL_REG_WDATA_LNK, br_type, MEM_X, 0, 0, 0)
+
+  private def j(r: Boolean = false, l: Int = 0): List[B] = d(if (r) SEL_ALU1_SA else SEL_ALU1_X, SEL_ALU2_X, SEL_IMM_J, FN_X, 0, 0, 0, l, SEL_REG_WADDR_31, SEL_REG_WDATA_LNK, BR_TYPE_X, MEM_X, 0, 0, 0)
 
   def decode(inst: UInt): this.type = {
     val decoder = DecodeLogic(inst, default, table)
