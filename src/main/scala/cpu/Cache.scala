67a2b7e04e20d4c39d735572e6619fada7cf6dd7
sinofp <sinofp@tuta.io>
2020-11-27 16:10:03 +0800

    Add pseudo LRU
    大概这样吧，反正能生成Verilog
    对不对也暂时无从验证，反正肉眼看没啥问题
    
    Chisel没有asBools的逆运算，很怪异
    
    接下来把存储器移出去，当然，debug用的内部版本还会保留

--- src/main/scala/cpu/Cache.scala
+++ src/main/scala/cpu/Cache.scala
@@ -12,7 +12,7 @@ class Cache extends MultiIOModule {
   val nSets = 16
   val nBlocks = 16
 
-  require(nWays % 2 == 0, "ways should be even")
+  require(nWays % 2 == 0 && nWays > 2, "ways should be even")
   require(nSets % 2 == 0, "sets should be even")
   require(nBlocks % 2 == 0, "blocks should be even")
 
@@ -51,15 +51,29 @@ class Cache extends MultiIOModule {
   })
 
   val cache_addr = addr.set ## addr.block
-  val hits = tagMem.map(_ (cache_addr) === addr.tag)
-  val hit = hits.reduce((a, b) => a | b)
+
+  // 伪LRU：
+  // 假设有4路cache
+  // idx   : 0 1 | 2 3
+  // hits  : f f | t f
+  // group :  f     t
+  // group代表本次使用的Cache是4路中的哪一群——为真时代表后半群
+  // 类似的，Used记录上次同样低地址，选出的是哪一组，所以U(cache_addr) := group
+  // 未命中，要换出时，通过U找哪一群上次被使用。如果是后半群（U为真），选中随机数+0的那一路，否则+halfWay
+  // 随机数在这里简单实现成了计数器
+
+  val halfWays = nWays / 2
+  val (roulette, _) = Counter(false.B, halfWays) // 有必要么？我一直“随机”第一个不行？
+  val hitWay = tagMem.map(_ (cache_addr) === addr.tag)
+  val hit = hitWay.reduce(_ | _)
+  val group = hitWay.drop(halfWays).reduce(_ | _)
+  val getOut = UIntToOH(roulette + Mux(U(cache_addr), 0.U, halfWays.asUInt)).asBools
+  // 对Seq[Bool]的土法Mux，因为Seq[Bool]不是Data，也没有asUInt之类的
+  val selWay = for (way <- 0 until nWays) yield Mux(hit, hitWay(way), getOut(way))
   cpu.miss := ~hit
 
   val sCompare :: sALLOCATE :: sWRITEBACK :: Nil = Enum(3)
   val state = RegInit(sCompare)
-
-  // 用于伪LRU，假设4路，分成两组，用U选其中一组，用它选这一组的哪一路要被换出去
-  val (roulette, _) = Counter(false.B, nWays / 2)
 
   // 这行么
   cpu.rdata := DontCare
@@ -68,17 +82,18 @@ class Cache extends MultiIOModule {
   switch(state) {
     is(sCompare) {
       when(hit) {
+        U(cache_addr) := group
         when(cpu.ren) {
-          cpu.rdata := Mux1H(hits, dataMem.map(_ (cache_addr)))
-          // todo update U
+          cpu.rdata := Mux1H(hitWay, dataMem.map(_ (cache_addr)))
         }.elsewhen(cpu.wen) {
-          Mux1H(hits, D)(addr.set) := true.B
-          Mux1H(hits, dataMem)(cache_addr) := cpu.wdata
+          Mux1H(selWay, D)(addr.set) := true.B
+          Mux1H(selWay, dataMem)(cache_addr) := cpu.wdata
         }
       }.elsewhen(cpu.ren | cpu.wen) {
         // cache miss，抓一组写回去
-        // todo 找不脏的一组直接分配
-        when(D.head(cache_addr)) {
+        // todo 找不脏的一组直接分配 —— 意义大么？
+        when(Mux1H(selWay, D)(cache_addr)) {
+          Mux1H(selWay, D)(cache_addr) := false.B
           mem.write.valid := true.B // = wen?
           state := sWRITEBACK
         }.otherwise {
@@ -88,17 +103,19 @@ class Cache extends MultiIOModule {
       }
     }
     is(sALLOCATE) {
+      U(cache_addr) := group
       when(mem.read.fire) {
         mem.addr := cpu.addr
-        dataMem.head(cache_addr) := mem.read.bits
+        Mux1H(selWay, dataMem)(cache_addr) := mem.read.bits
       }
       mem.read.ready := false.B
       state := sCompare
     }
     is(sWRITEBACK) {
+      U(cache_addr) := group
       when(mem.write.fire) {
         mem.addr := cpu.addr
-        mem.write.bits := dataMem.head(cache_addr)
+        mem.write.bits := Mux1H(selWay, dataMem)(cache_addr)
       }
       mem.write.valid := false.B
       mem.read.ready := true.B
